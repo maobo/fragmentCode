36633583cd9b3a28fc7d2c705fcb9ced679d8811
Maobo <maobo@live.cn>
2023-05-17 21:33:49 +0800

    init

---
+++ src/main/java/cn/mojit/fragmentcode/FragmentCodeApplication.java
@@ -0,0 +1,13 @@
+package cn.mojit.fragmentcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FragmentCodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FragmentCodeApplication.class, args);
+	}
+
+}
